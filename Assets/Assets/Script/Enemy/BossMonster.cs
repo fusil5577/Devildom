a5dde05a74d96002098ed138f8374885bb763bc7
TAEYOUNG0216 <xodud5607@gmail.com>
2024-06-24 05:35:57 +0900

    feat 보스 에셋

---
+++ Assets/Assets/Script/Enemy/BossMonster.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMonster : MonoBehaviour
+{
+
+    public class BossSpawner : MonoBehaviour
+    {
+        public GameObject bossPrefab;
+        public Transform[] spawnPoints;
+
+
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
